e96a9e17ff91739e27dc288682a7e987cf257f95
yt-kobayashi <270yoshikoba@gmail.com>
2020-11-14 01:14:36 +0900

    [ADD] VisualStudioプロジェクト作成

---
+++ XMLStreamWrapper/XMLStreamWrapper/XmlStreamWrapper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace XMLStreamWrapper
+{
+    public class XmlSerializerWrapper<ObjectType>
+    {
+        public XmlSerializer Serializer { get; set; }       // Xmlシリアライザ
+        public string FileName { get; set; }                // 保存先ファイル名
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="type"></param>
+        public XmlSerializerWrapper(in ObjectType serializeTarget, in string fileName)
+        {
+            Serializer = new XmlSerializer(typeof(ObjectType));
+            FileName = fileName;
+        }
+    }
+}
